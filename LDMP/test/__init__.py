b8b8198f8a8c487fe063921052ed36ebf2f3104d
John G <gkahiu@gmail.com>
2022-03-16 11:22:25 +0300

    Comments old test code.

--- LDMP/test/__init__.py
+++ LDMP/test/__init__.py
@@ -20,12 +20,12 @@ from qgis.PyQt.QtWidgets import QMessageBox, QApplication
 from qgis.PyQt.QtTest import QTest
 
 # from LDMP.layers import add_layer, get_file_metadata
-
-
-with open(os.path.join(os.path.dirname(__file__), 'trends.earth_test_user_credentials.json'), 'r') as fin:
-    regular_keys = json.load(fin)
-with open(os.path.join(os.path.dirname(__file__), 'trends.earth_admin_user_credentials.json'), 'r') as fin:
-    admin_keys = json.load(fin)
+#
+#
+# with open(os.path.join(os.path.dirname(__file__), 'trends.earth_test_user_credentials.json'), 'r') as fin:
+#     regular_keys = json.load(fin)
+# with open(os.path.join(os.path.dirname(__file__), 'trends.earth_admin_user_credentials.json'), 'r') as fin:
+#     admin_keys = json.load(fin)
 
 
 # Ensure any message boxes that open are closed within 1 second
@@ -34,9 +34,11 @@ def close_msg_boxes():
         if isinstance(w, QMessageBox):
             print('Closing message box')
             QTest.keyClick(w, Qt.Key_Enter)
-timer = QTimer()
-timer.timeout.connect(close_msg_boxes)
-timer.start(1000)
+
+
+# timer = QTimer()
+# timer.timeout.connect(close_msg_boxes)
+# timer.start(1000)
 
 
 # Used to load default bands from test datasets onto the map
