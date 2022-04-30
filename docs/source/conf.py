209efbff0ee163253b0a27a856fa89d4e82d37f5
John G <gkahiu@gmail.com>
2022-04-30 12:49:11 +0300

    Clear html static path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -207,7 +207,7 @@ html_favicon = "trends_earth_logo_square_32x32.ico"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = []
 
 html_show_sourcelink = False
 
